9cc8b81b58b0c1ec17b56718d66e27b02418d39e
edgar poce <edgar.ponce@rackspace.com>
2020-04-30 19:15:14 -0300

    back to original jest coverage config

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   "coverageThreshold": {
     "global": {
-      "branches": 70,
+      "branches": 80,
       "functions": 80,
       "lines": 80,
-      "statements": 80
+      "statements": -10
     }
   },
   "roots": [
